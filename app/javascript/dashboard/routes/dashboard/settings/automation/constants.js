e08a0524586f8c36f15050db9c1c231dd871df91
redloro <nabil@fe.ro>
2024-06-30 16:38:51 -0400

    fix:add delete_contact, delete_conversation automation actions

--- app/javascript/dashboard/routes/dashboard/settings/automation/constants.js
+++ app/javascript/dashboard/routes/dashboard/settings/automation/constants.js
@@ -88,6 +88,11 @@ export const AUTOMATIONS = {
         attributeI18nKey: 'SEND_EMAIL_TRANSCRIPT',
       },
       {
+        key: 'delete_contact',
+        name: 'Delete contact',
+        attributeI18nKey: 'DELETE_CONTACT',
+      },
+      {
         key: 'mute_conversation',
         name: 'Mute conversation',
         attributeI18nKey: 'MUTE_CONVERSATION',
@@ -102,6 +107,11 @@ export const AUTOMATIONS = {
         key: 'resolve_conversation',
         name: 'Resolve conversation',
         attributeI18nKey: 'RESOLVE_CONVERSATION',
+      },
+      {
+        key: 'delete_conversation',
+        name: 'Delete conversation',
+        attributeI18nKey: 'DELETE_CONVERSATION',
       },
       {
         key: 'send_webhook_event',
@@ -220,6 +230,11 @@ export const AUTOMATIONS = {
         attributeI18nKey: 'SEND_EMAIL_TRANSCRIPT',
       },
       {
+        key: 'delete_contact',
+        name: 'Delete contact',
+        attributeI18nKey: 'DELETE_CONTACT',
+      },
+      {
         key: 'mute_conversation',
         name: 'Mute conversation',
         attributeI18nKey: 'MUTE_CONVERSATION',
@@ -233,6 +248,11 @@ export const AUTOMATIONS = {
         key: 'resolve_conversation',
         name: 'Resolve conversation',
         attributeI18nKey: 'RESOLVE_CONVERSATION',
+      },
+      {
+        key: 'delete_conversation',
+        name: 'Delete conversation',
+        attributeI18nKey: 'DELETE_CONVERSATION',
       },
       {
         key: 'send_webhook_event',
@@ -365,6 +385,11 @@ export const AUTOMATIONS = {
         attributeI18nKey: 'SEND_EMAIL_TRANSCRIPT',
       },
       {
+        key: 'delete_contact',
+        name: 'Delete contact',
+        attributeI18nKey: 'DELETE_CONTACT',
+      },
+      {
         key: 'mute_conversation',
         name: 'Mute conversation',
         attributeI18nKey: 'MUTE_CONVERSATION',
@@ -378,6 +403,11 @@ export const AUTOMATIONS = {
         key: 'resolve_conversation',
         name: 'Resolve conversation',
         attributeI18nKey: 'RESOLVE_CONVERSATION',
+      },
+      {
+        key: 'delete_conversation',
+        name: 'Delete conversation',
+        attributeI18nKey: 'DELETE_CONVERSATION',
       },
       {
         key: 'send_webhook_event',
