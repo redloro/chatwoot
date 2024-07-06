ff42c0d7d96c391163f63bb1bbfed22569941675
redloro <nabil@fe.ro>
2024-07-06 17:34:34 -0400

    fix:articles draft mode

--- app/javascript/dashboard/routes/dashboard/helpcenter/components/Header/EditArticleHeader.vue
+++ app/javascript/dashboard/routes/dashboard/helpcenter/components/Header/EditArticleHeader.vue
@@ -79,7 +79,7 @@
             ref="arrowDownButton"
             size="small"
             icon="chevron-down"
-            :is-disabled="!articleSlug || isArchivedArticle"
+            :is-disabled="!articleSlug"
             @click="openActionsDropdown"
           />
         </div>
@@ -96,8 +96,19 @@
                 size="small"
                 icon="book-clock"
                 @click="updateArticleStatus(ARTICLE_STATUS_TYPES.ARCHIVE)"
+                v-if="!isArchivedArticle"
               >
                 {{ $t('HELP_CENTER.EDIT_HEADER.MOVE_TO_ARCHIVE_BUTTON') }}
+              </woot-button>
+              <woot-button
+                variant="clear"
+                color-scheme="secondary"
+                size="small"
+                icon="book-compass"
+                @click="updateArticleStatus(ARTICLE_STATUS_TYPES.DRAFT)"
+                v-if="!isDraftArticle"
+              >
+                {{ $t('HELP_CENTER.EDIT_HEADER.MOVE_TO_DRAFT_BUTTON') }}
               </woot-button>
             </woot-dropdown-item>
           </woot-dropdown-menu>
@@ -170,6 +181,9 @@ export default {
     isArchivedArticle() {
       return this.currentArticleStatus === 'archived';
     },
+    isDraftArticle() {
+      return this.currentArticleStatus === 'draft';
+    },
   },
   methods: {
     onClickGoBack() {
@@ -191,10 +205,12 @@ export default {
         this.$emit('update-meta');
         this.statusUpdateSuccessMessage(status);
         this.closeActionsDropdown();
-        if (status === this.ARTICLE_STATUS_TYPES.ARCHIVE) {
-          this.$track(PORTALS_EVENTS.ARCHIVE_ARTICLE, { uiFrom: 'header' });
-        } else if (status === this.ARTICLE_STATUS_TYPES.PUBLISH) {
+        if (status === this.ARTICLE_STATUS_TYPES.PUBLISH) {
           this.$track(PORTALS_EVENTS.PUBLISH_ARTICLE);
+        } else if (status === this.ARTICLE_STATUS_TYPES.ARCHIVE) {
+          this.$track(PORTALS_EVENTS.ARCHIVE_ARTICLE, { uiFrom: 'header' });
+        } else if (status === this.ARTICLE_STATUS_TYPES.DRAFT) {
+          this.$track(PORTALS_EVENTS.DRAFT_ARTICLE, { uiFrom: 'header' });
         }
       } catch (error) {
         this.alertMessage =
@@ -208,6 +224,8 @@ export default {
         this.alertMessage = this.$t('HELP_CENTER.PUBLISH_ARTICLE.API.SUCCESS');
       } else if (status === this.ARTICLE_STATUS_TYPES.ARCHIVE) {
         this.alertMessage = this.$t('HELP_CENTER.ARCHIVE_ARTICLE.API.SUCCESS');
+      } else if (status === this.ARTICLE_STATUS_TYPES.DRAFT) {
+        this.alertMessage = this.$t('HELP_CENTER.DRAFT_ARTICLE.API.SUCCESS');
       }
     },
     statusUpdateErrorMessage(status) {
@@ -215,6 +233,8 @@ export default {
         this.alertMessage = this.$t('HELP_CENTER.PUBLISH_ARTICLE.API.ERROR');
       } else if (status === this.ARTICLE_STATUS_TYPES.ARCHIVE) {
         this.alertMessage = this.$t('HELP_CENTER.ARCHIVE_ARTICLE.API.ERROR');
+      } else if (status === this.ARTICLE_STATUS_TYPES.DRAFT) {
+        this.alertMessage = this.$t('HELP_CENTER.DRAFT_ARTICLE.API.ERROR');
       }
     },
     openSidebar() {
